de25bc45c83665e0fecd1a3243911041aaef3b12
Ayush <135319056+ayussh-2@users.noreply.github.com>
2025-11-04 20:58:29 +0530

    Update and rename firebase-config.js to firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -9,4 +10,4 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,};
 
 const app = initializeApp(firebaseConfig);
-export {app};
+export const db = getFirestore(app);
